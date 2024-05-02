d4c3c9bc69596a68a68a0f634787398d24a9337b
srgrusso <srgrusso240@gmail.com>
2024-05-01 20:46:27 -0500

    Revert "drivers: kernelsu: fix secctx mem leak"
    This reverts commit 6924d2a8fd297f793976298e0bd2af5f2e92cee3.

--- drivers/kernelsu/selinux/selinux.c
+++ drivers/kernelsu/selinux/selinux.c
@@ -103,14 +103,11 @@ bool is_ksu_domain()
 {
 	char *domain;
 	u32 seclen;
-	bool result;
 	int err = security_secid_to_secctx(current_sid(), &domain, &seclen);
 	if (err) {
 		return false;
 	}
-	result = strncmp(KERNEL_SU_DOMAIN, domain, seclen) == 0;
-	security_release_secctx(domain, seclen);
-	return result;
+	return strncmp(KERNEL_SU_DOMAIN, domain, seclen) == 0;
 }
 
 bool is_zygote(void *sec)
@@ -121,12 +118,9 @@ bool is_zygote(void *sec)
 	}
 	char *domain;
 	u32 seclen;
-	bool result;
 	int err = security_secid_to_secctx(tsec->sid, &domain, &seclen);
 	if (err) {
 		return false;
 	}
-	result = strncmp("u:r:zygote:s0", domain, seclen) == 0;
-	security_release_secctx(domain, seclen);
-	return result;
+	return strncmp("u:r:zygote:s0", domain, seclen) == 0;
 }
